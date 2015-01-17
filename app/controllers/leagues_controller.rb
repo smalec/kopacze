0553c3553825f0aae6943e4e254e535caaedcd44
smalec <kaczy2406@gmail.com>
2015-01-17 10:59:22 +0100

    Advanced league creation

--- app/controllers/leagues_controller.rb
+++ app/controllers/leagues_controller.rb
@@ -24,11 +24,39 @@ class LeaguesController < ApplicationController
 
   def create
     @league = League.new(league_params)
+
+    @towns = Town.all
+    town = @league.town
+    lowest_div = town.leagues.sort_by{:division}[-1]
+    if lowest_div == nil
+      @league.division = 0
+      @league.group = 1
+    else
+      lowest_div = lowest_div[:division]
+      if lowest_div == 0
+        @league.division = 1
+        @league.group = 1
+      elsif lowest_div == 1
+        @league.division = 2
+        @league.group = 1
+      else
+        groups_in_lowest_div = town.leagues.select{|league| league.division == lowest_div}.count
+        if groups_in_lowest_div == 2**(lowest_div - 1)
+          @league.division = lowest_div + 1
+          @league.group = 1
+        else
+          @league.division = lowest_div
+          @league.group = groups_in_lowest_div + 1
+        end
+      end
+    end
+
     @league.save
     respond_with(@league)
   end
 
   def update
+    @towns = Town.all
     @league.update(league_params)
     respond_with(@league)
   end
--- app/models/league.rb
+++ app/models/league.rb
@@ -1,9 +1,6 @@
 class League < ActiveRecord::Base
   belongs_to  :town
 
-  validates :division, :numericality => { :greater_than_or_equal_to => 0 }
-  validates :group, :numericality => { :greater_than => 0 }
-
   def description
     if division == 0
       return "Ekstraklasa"
